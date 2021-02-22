d41a58c0ea48b38bb2944f89844f9f49af545084
Liam Appelbe <liamappelbe@gmail.com>
2021-02-21 16:43:45 -0800

    App is 80% of the way to being usable.
    All the plumbing is in place, and the embedder is trained. Remaining
    tasks are to integrate the embedder into the app using tflite, and build
    the key generation UI.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Deep Defender
-Real-time defense against deep fakes.
+Real-time defense against deep fakes and malicious editing.
+
+## ML notes
+- Data set: https://www.kaggle.com/mozillaorg/common-voice
+- Retrieved on 15/02/2021
+- The data set was preprocessed by running embedder/clean.py on every wav file
+- Then the model was trained using embedder/train.py
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
+import 'dart:typed_data';
+import 'package:crypto_keys/crypto_keys.dart' show PrivateKey, KeyPair;
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr/qr.dart';
+import 'audio_hasher.dart';
+import 'byte_signer.dart';
+import 'code_builder.dart';
+import 'metadata.dart';
+import 'microphone.dart';
+import 'tf_embedder.dart';
+
+AudioHasher getAudioHasher() {
+  return TfEmbedder();
+}
+
+PrivateKey getPrivateKey() {
+  // TODO: Generate and store a key pair. Build a UI to share the public key.
+  var keyPair = new KeyPair.fromJwk({
+  "kty": "oct",
+  "k": "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75" +
+      "aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow"
+  });
+  return keyPair.privateKey;
+}
 
 void main() {
-  runApp(DeepDefenderApp());
+  runApp(DeepDefenderApp(CodeBuilder(
+      Metadata(), getAudioHasher(), ByteSigner(getPrivateKey()))));
 }
 
 class DeepDefenderApp extends StatelessWidget {
+  final CodeBuilder _codeBuilder;
+  DeepDefenderApp(this._codeBuilder) {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,23 +42,35 @@ class DeepDefenderApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: RunningPage(title: 'Deep Defender'),
+      home: RunningPage(title: 'Deep Defender', codeBuilder: _codeBuilder),
     );
   }
 }
 
 class RunningPage extends StatefulWidget {
-  RunningPage({Key key, this.title}) : super(key: key);
-
   final String title;
+  final CodeBuilder codeBuilder;
+  RunningPage({Key key, this.title, this.codeBuilder}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState(codeBuilder);
 }
 
 class _MyHomePageState extends State<RunningPage> {
-  void _incrementCounter() {
+  final CodeBuilder _codeBuilder;
+  Uint8List _code;
+  String _info = "Waiting to hear from the microphone...";
+  Future<Microphone> _microphone;
+  _MyHomePageState(this._codeBuilder) {
+    _microphone = Microphone.mic(_updateCode);
+  }
+
+  void _updateCode(int timeMs, Float64List audio) {
     setState(() {
+      _code = _codeBuilder.generate(timeMs, audio);
+      int dt = DateTime.now().millisecondsSinceEpoch - timeMs;
+      double cpu = dt / 1e3 / Microphone.kRefreshTime - 1;
+      _info = "Latency: ${dt}ms\nCPU: ${(100 * cpu).toStringAsFixed(2)}%";
     });
   }
 
@@ -44,9 +84,15 @@ class _MyHomePageState extends State<RunningPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            QrImage(
-              data: "1234567890",
-              version: QrVersions.auto,
+            if (_code != null) QrImage.withQr(
+              qr: QrCode.fromUint8List(
+                data: _code,
+                errorCorrectLevel: QrErrorCorrectLevel.L,
+              )..make(),
+            ),
+            Text(
+              _info,
+              textAlign: TextAlign.center,
             ),
           ],
         ),
